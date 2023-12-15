c7552d8e6388c1eb904d996b2322839aa2cd1bfc
csf22mnc <conormcnulty1997@gmail.com>
2023-12-15 08:56:39 +0000

    successfuly created database visble on the maps

--- app/src/main/java/com/example/mapsassignment/EventDetailsActivity.java
+++ app/src/main/java/com/example/mapsassignment/EventDetailsActivity.java
@@ -15,25 +15,24 @@ public class EventDetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_details);
 
-        // Retrieve the CleaningEvent from the intent
-        CleaningEvent clickedEvent = getIntent().getParcelableExtra("updatedEvent");
+        // Find your TextViews in the layout
+        TextView eventNameTextView = findViewById(R.id.textViewEventName);
+        TextView eventDateTextView = findViewById(R.id.textViewEventDate);
+        TextView eventTimeTextView = findViewById(R.id.textViewEventTime);
+        TextView eventLocationTextView = findViewById(R.id.textViewEventLocation);
+        TextView eventGoalTextView = findViewById(R.id.textViewEventGoal);
 
-        // Populate UI components with event details
-        if (clickedEvent != null) {
-            TextView textViewEventName = findViewById(R.id.textViewEventName);
-            TextView textViewEventDate = findViewById(R.id.textViewEventDate);
-            TextView textViewEventTime = findViewById(R.id.textViewEventTime);
-            TextView textViewEventLocation = findViewById(R.id.textViewEventLocation);
-            TextView textViewEventGoal = findViewById(R.id.textViewEventGoal);
+        // Retrieve the clicked event from the Intent
+        Intent intent = getIntent();
+        CleaningEvent clickedEvent = intent.getParcelableExtra("clickedEvent");
 
-            textViewEventName.setText("Event Name: " + clickedEvent.getEventName());
-            textViewEventDate.setText("Event Date: " + clickedEvent.getFormattedDate());
-            textViewEventTime.setText("Event Time: " + clickedEvent.getFormattedTime());
-            textViewEventLocation.setText("Event Location: " + clickedEvent.getEventLocation());
-            textViewEventGoal.setText("Event Goal: " + clickedEvent.getEventGoal());
-        }
+        // Now use 'clickedEvent' to update your UI elements
+        eventNameTextView.setText("Event Name: " + clickedEvent.getEventName());
+        eventDateTextView.setText("Event Date: " + clickedEvent.getEventDate());
+        eventTimeTextView.setText("Event Time: " + clickedEvent.getEventTime());
+        eventLocationTextView.setText("Event Location: " + clickedEvent.getEventLocation());
+        eventGoalTextView.setText("Event Goal: " + clickedEvent.getEventGoal());
     }
-
 
     // Add the following method to save the event and send it back to MapsActivity
     private void saveEventAndFinish(CleaningEvent updatedEvent) {
@@ -42,10 +41,6 @@ public class EventDetailsActivity extends AppCompatActivity {
         setResult(Activity.RESULT_OK, resultIntent);
         finish();
     }
-
-
-    // This method can be called when saving the event in your activity
-    // You can call this method passing the updated event
-    // Example: saveEventAndFinish(updatedEvent);
 }
+
 
--- app/src/main/java/com/example/mapsassignment/MapsActivity.java
+++ app/src/main/java/com/example/mapsassignment/MapsActivity.java
@@ -51,16 +51,8 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
-        // Find the map fragment and get a reference to the map
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-
-        // Set a click listener for the map after it's ready
-        mapFragment.getMapAsync(this);
-
         // Location manager setup
         locationHandler = new LocationHandler(this, new MyLocationListener(this, mMap));
-
         locationHandler.startLocationUpdates();
 
         // Check and request location permissions
@@ -76,14 +68,13 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             permissionGranted = true;
         }
 
+        // Find the map fragment and get a reference to the map
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+
+        // Set a click listener for the map after it's ready
+        mapFragment.getMapAsync(this);
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        // Stop location updates when the activity is paused
-        locationHandler.stopLocationUpdates();
-    }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -92,8 +83,19 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             CleaningEvent updatedEvent = data.getParcelableExtra("updatedEvent");
 
             if (updatedEvent != null) {
-                // Update the map with the updated event
-                updateMapWithCleaningEvents();
+                // Add the new marker for the updated event
+                LatLng eventLocation = updatedEvent.getLocation();
+                if (eventLocation != null) {
+                    MarkerOptions markerOptions = new MarkerOptions()
+                            .position(eventLocation)
+                            .title(updatedEvent.getEventName());
+
+                    Marker marker = mMap.addMarker(markerOptions);
+                    marker.setTag(updatedEvent);
+
+                    // Center the map camera on the new marker
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(eventLocation, 12.0f));
+                }
 
                 Toast.makeText(this, "Event saved successfully", Toast.LENGTH_SHORT).show();
             } else {
@@ -132,33 +134,44 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
             @Override
             public View getInfoContents(Marker marker) {
+                // Inflate the custom info window layout
                 View view = getLayoutInflater().inflate(R.layout.custom_info_window, null);
 
-                TextView titleTextView = view.findViewById(R.id.infoWindowTitle);
-                TextView snippetTextView = view.findViewById(R.id.infoWindowSnippet);
-
-                // Retrieve the CleaningEvent associated with the marker
+                // Retrieve the CleaningEvent associated with the clicked marker
                 CleaningEvent event = (CleaningEvent) marker.getTag();
-
-                // Debugging: Log the event details
-                Log.d("InfoContents", "Event: " + event);
-                Log.d("InfoContents", "EventName: " + event.getEventName());
-                Log.d("InfoContents", "EventDate: " + event.getEventDate());
-                Log.d("InfoContents", "EventTime: " + event.getEventTime());
 
                 // Set custom information in the info window
                 if (event != null) {
+                    TextView titleTextView = view.findViewById(R.id.infoWindowTitle);
+                    TextView snippetTextView = view.findViewById(R.id.infoWindowEventDate); // or any other correct ID
+
+
                     titleTextView.setText(event.getEventName());
                     snippetTextView.setText("Date: " + event.getEventDate() + "\nTime: " + event.getEventTime());
                 }
 
                 return view;
             }
-
         });
     }
+    private class InfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
 
+        private View view;
 
+        InfoWindowAdapter(View view) {
+            this.view = view;
+        }
+
+        @Override
+        public View getInfoWindow(Marker marker) {
+            return null; // Use the default info window frame
+        }
+
+        @Override
+        public View getInfoContents(Marker marker) {
+            return view; // Use the custom info window layout
+        }
+    }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
@@ -178,45 +191,76 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             Marker marker = mMap.addMarker(new MarkerOptions().position(eventLocation).title(eventName));
             marker.setTag(event);
         }
-        mMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
-            @Override
-            public View getInfoWindow(Marker marker) {
-                return null; // Use default info window frame
-            }
 
+        // Set marker click listener
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
             @Override
-            public View getInfoContents(Marker marker) {
-                // Inflate the custom info window layout
-                View view = getLayoutInflater().inflate(R.layout.custom_info_window, null);
-
+            public boolean onMarkerClick(Marker marker) {
                 // Retrieve the CleaningEvent associated with the clicked marker
                 CleaningEvent clickedEvent = (CleaningEvent) marker.getTag();
 
-                // Populate the custom info window with event details
                 if (clickedEvent != null) {
-                    TextView titleTextView = view.findViewById(R.id.infoWindowTitle);
-                    TextView snippetTextView = view.findViewById(R.id.infoWindowSnippet);
+                    // Inflate the custom info window layout
+                    View view = getLayoutInflater().inflate(R.layout.custom_info_window, null);
 
-                    titleTextView.setText(clickedEvent.getEventName());
-                    snippetTextView.setText(clickedEvent.getEventDate() + " " + clickedEvent.getEventTime());
+                    // Find views in the custom info window layout
+                    TextView eventNameTextView = view.findViewById(R.id.infoWindowTitle);
+                    TextView eventDateTextView = view.findViewById(R.id.infoWindowEventDate);
+                    TextView eventTimeTextView = view.findViewById(R.id.infoWindowEventTime);
+                    TextView eventLocationTextView = view.findViewById(R.id.infoWindowEventLocation);
+                    TextView eventGoalTextView = view.findViewById(R.id.infoWindowEventGoal);
+
+
+                    // Set text in the views
+                    eventNameTextView.setText("Event Name: " + clickedEvent.getEventName());
+                    eventDateTextView.setText("Event Date: " + clickedEvent.getEventDate());
+                    eventTimeTextView.setText("Event Time: " + clickedEvent.getEventTime());
+                    eventLocationTextView.setText("Event Location: " + clickedEvent.getEventLocation());
+                    eventGoalTextView.setText("Event Goal: " + clickedEvent.getEventGoal());
+
+                    // Create and show the custom info window
+                    InfoWindowAdapter adapter = new InfoWindowAdapter(view);
+                    mMap.setInfoWindowAdapter(adapter);
+
+                    // Show the info window for the clicked marker
+                    marker.showInfoWindow();
                 }
 
-                return view;
+                // Return true to consume the click event
+                return true;
+            }
+
+        });
+
+        // Set marker click listener
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                // Retrieve the CleaningEvent associated with the clicked marker
+                CleaningEvent clickedEvent = (CleaningEvent) marker.getTag();
+
+                // If you have an EventDetailsActivity, launch it and pass necessary data
+                if (clickedEvent != null) {
+                    Intent intent = new Intent(MapsActivity.this, EventDetailsActivity.class);
+                    intent.putExtra("clickedEvent", clickedEvent);
+                    startActivity(intent);
+                }
+
+                // Return true to consume the click event
+                return true;
             }
         });
+
         // Handle map click events
         mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             @Override
             public void onMapClick(LatLng point) {
-                Log.d("MapClick", "onMapClick called");
-
                 // Check if cleaningEvents is not null
                 if (cleaningEvents == null) {
                     Log.e("MapClick", "cleaningEvents is null");
                     return;
                 }
 
-                Log.d("MapClick", "cleaningEvents size: " + cleaningEvents.size());
                 CleaningEvent newEvent = new CleaningEvent();
                 newEvent.setLocation(point);
 
@@ -234,49 +278,21 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
                     // Check if eventDate and eventTime are not null before combining
                     if (eventDate != null && eventTime != null) {
-                        newEvent.setDateTime(newEvent.combineDateTime(eventDate, eventTime)); // Assuming combineDateTime is in CleaningEvent
+                        newEvent.setDateTime(newEvent.combineDateTime(eventDate, eventTime));
 
-                        // ... (rest of your code)
-
+                        startActivityForResult(intent, ADD_EVENT_REQUEST_CODE);
                     } else {
                         Log.e("MapClick", "eventDate or eventTime is null");
                         // Handle the case where either eventDate or eventTime is null
                     }
-
-                    startActivityForResult(intent, ADD_EVENT_REQUEST_CODE);
                 } else {
                     Log.e("MapClick", "New event location is null");
                 }
             }
-
-
-
-        });
-
-        // Set marker click listener
-        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
-            // MapsActivity.java
-            @Override
-            public boolean onMarkerClick(Marker marker) {
-                // Retrieve the CleaningEvent associated with the clicked marker
-                CleaningEvent clickedEvent = (CleaningEvent) marker.getTag();
-
-                // If you have an EventDetailsActivity, launch it and pass necessary data
-                if (clickedEvent != null) {
-                    Intent intent = new Intent(MapsActivity.this, EventDetailsActivity.class);
-                    intent.putExtra("clickedEvent", clickedEvent);
-                    startActivity(intent);
-                }
-
-                // Return true to consume the click event
-                return true;
-            }
-
         });
     }
-
-
 }
+
 
 
 
